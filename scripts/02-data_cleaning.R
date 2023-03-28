e16c4c16e8d8b52fc686c7257197a5ebf0abf334
MissyZhang <123055808+MissyZhang@users.noreply.github.com>
2023-03-27 23:11:23 -0400

    updates

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,26 +1,26 @@
 #### Preamble ####
-# Purpose: Cleans.... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Data: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Clean the raw data
+# Author: Missy Zhang
+# Data: 27 March 2023
+# Contact: mengze.zhang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Clean data ####
-# [...UPDATE THIS...]
 raw_data <- read_csv("inputs/data/raw_data.csv")
 
-
+cleaned_data <- subset(raw_data, !(`Operational Activity` %in% c("On-board", "Off-board"))) |>
+  select(Industry,
+         Year,
+         'Injury Frequency Rate (6)') |>
+  rename(injury_frequency_rate = 'Injury Frequency Rate (6)')
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change cleaned_data to whatever name you end up with at the end of cleaning
-write_csv(cleaned_data, "outputs/data/cleaned_data.csv")
+write_csv(cleaned_data, "inputs/data/cleaned_data.csv")
 
